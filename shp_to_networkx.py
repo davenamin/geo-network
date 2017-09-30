816a1c2d87600a9498d223394c6cec5711174acd
davenamin <amind@my.uri.edu>
2017-09-30 17:42:23 -0400

    reorganized logic and added distance attributes to edges

--- shp_to_networkx.py
+++ shp_to_networkx.py
@@ -1,62 +1,112 @@
 import geopandas as gpd
 import shapely as sh
 import networkx as nx
+from collections import defaultdict
+import pyproj
+from functools import partial
+import matplotlib.pyplot as plt
 
 file = "./test-geometry/test-geometry.shp"
 
-buffersize = .00000000005  # set the buffer around the line to search for intersections.  should be < 1e-6
+# projections for coordinates (lat/lon) and distances
+# https://epsg.io/4326
+crs_gps = "+proj=longlat +datum=WGS84 +no_defs"
+
+# https://epsg.io/3654
+crs_distance = "+proj=tmerc +lat_0=41.08333333333334 +lon_0=-71.5 +k=0.99999375 +x_0=99999.99998983997 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=us-ft +no_defs"
+
+buffersize = 5e-11  # set the buffer around the line to search for intersections.  should be < 1e-6
 
 # get GeoDataFrame of linestrings
 gdf = gpd.read_file(file)
-
-# add column for the subgraph of each linestring
-gdf['graph'] = gdf.apply(lambda x: nx.Graph(), axis=1)
-
+gdf = gdf.to_crs(crs_gps)
 # initialize the main graph
 G = nx.Graph()
 
-# start a node counter
-next_node = 0
-
-# build the subgraphs for each line
-for m in range(0, len(gdf)):
-    s = gdf.iloc[m]  # placeholder for geometry analysis
-    # collect base nodes
-    pts = list(gdf.iloc[m].geometry.coords)
-    for p in pts:
-        gdf.graph.iloc[m].add_node(next_node, point=sh.geometry.Point(p))  # TODO: streamline with a placeholder graph so we're not selecting from gdf every time
-        G.add_node(next_node, point=sh.geometry.Point(p))
-        next_node = next_node+1
-
-    for n in range(m+1, len(gdf)):  # iterate through all the upcoming lines
-        t = gdf.iloc[n]  # placeholder for geometry analysis
-
-        if t.geometry.intersects(s.geometry.buffer(buffersize)):  # check for intersection or near-touching
-            overlap = t.geometry.intersection(s.geometry.buffer(buffersize))
-            end_buffer = t.geometry.length - (2*buffersize)  # calculate the buffer zone around the end of the line
-
-            # handle geomoetry collections, and turn everything into list of geometry
-            if overlap.geom_type == 'GeometryCollection':  # if multiple intersections were returned, make a list
-                overlap = list(overlap)
-            else:  # else convert to a single element list
-                overlap = [overlap]
-
-            # get a representative point for the geometry and add nodes to graphs
-            for o in overlap:
-                o_pt = o.representative_point()  # FIXME: this will result in some weird and close node placements if intersection is an endpoint (clean up big graph?)
-                o_proj = s.geometry.project(o_pt)
-                int_point = s.geometry.interpolate(o_proj)
-                gdf.graph.iloc[m].add_node(next_node, point=int_point)
-                gdf.graph.iloc[n].add_node(next_node, point=int_point)
-                G.add_node(next_node, point=int_point)
-                next_node = next_node+1
-
-    # compute edge distances
+# helper structures for converting intersections and endpoints to nodes
+node_counter = 0
+pt_to_node = {}
 
 
+def node_tuple(pt):
+    """return the cached node_id and rounded off point for a given coord"""
+    precision = 8
+    global pt_to_node
+    global node_counter
+    pt_tuple = (round(pt[0], precision),
+                round(pt[1], precision))
+    if pt_tuple not in pt_to_node:
+        pt_to_node[pt_tuple] = node_counter
+        node_counter += 1
+    return pt_to_node[pt_tuple], pt_tuple
+
+# helper structure for tying node ids to rows in the geo dataframe
+row_to_nodes = defaultdict(set)
 
 
+def add_to_graph(coord, row, row2=None):
+    """centralize logic to add a new node to the graph 
+    and tie it to a row in the dataframe"""
+    global G
+    global node_to_row
+    node_id, node_pt = node_tuple(coord)
+    G.add_node(node_id, point=sh.geometry.Point(node_pt))
+    row_to_nodes[row].add(node_id)
+    if row2 is not None:
+        row_to_nodes[row2].add(node_id)
 
-# gdf.iloc[0].geometry.project(gdf.iloc[2].geometry.intersection(gdf.iloc[0].geometry))
-# gdf.iloc[0].geometry.interpolate(gdf.iloc[0].geometry.project(gdf.iloc[2].geometry.intersection(gdf.iloc[0].geometry))_
+for row1 in range(len(gdf)):
+    cur_line = gdf.iloc[row1].geometry
+    cur_line_buffered = cur_line.buffer(buffersize)
+    # add the start and end of this linestring as nodes
+    cur_start = cur_line.coords[0]
+    cur_end = cur_line.coords[-1]
+    add_to_graph(cur_start, row1)
+    add_to_graph(cur_end, row1)
+    for row2 in range(row1+1, len(gdf)):
+        other_line = gdf.iloc[row2]
+        # check for intersection or near-touching
+        if other_line.geometry.intersects(cur_line_buffered):
+            overlap = other_line.geometry.intersection(cur_line_buffered)
+            try:
+                overlap_list = list(overlap)
+            except TypeError:  # only had a single intersection
+                overlap_list = [overlap]
+            overlap_pts = [obj.centroid.coords[0] for obj in overlap_list]
+            for pt in overlap_pts:
+                add_to_graph(pt, row1, row2)
 
+
+# project the coordinates to get useful distances
+# http://toblerity.org/shapely/manual.html#other-transformations
+
+transform_point = partial(pyproj.transform,
+                          pyproj.Proj(crs_gps),
+                          pyproj.Proj(crs_distance))
+
+# now the graph contains nodes for all endpoints and intersections...
+# add edges across the nodes which comprise a linesegment from the original gdf
+for row, nodes in row_to_nodes.items():
+    cur_line = sh.ops.transform(transform_point,
+                                gdf.iloc[row].geometry)
+    projected_distances = [
+        (id, cur_line.project(sh.ops.transform(transform_point,
+                                               G.nodes[id]['point']),
+                              normalized=False))
+        for id in nodes]
+    projected_distances.sort(key=lambda x: x[1])
+    for ix in range(1, len(projected_distances)):
+        node1, dist1 = projected_distances[ix-1]
+        node2, dist2 = projected_distances[ix-2]
+        G.add_edge(node1, node2,
+                   distance=round(dist2-dist1),
+                   row=row,
+                   linestring=gdf.iloc[row].geometry)
+
+# plot for debugging
+for u, v in G.edges:
+    u_x, u_y = G.nodes[u]['point'].coords[0]
+    v_x, v_y = G.nodes[v]['point'].coords[0]
+    plt.plot([u_x, v_x], [u_y, v_y], alpha=.8, linestyle=(0, (1, 10)))
+
+plt.show()
